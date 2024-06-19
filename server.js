6987237a3542f6a2f9d9f74c771b665e268d421f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-19 16:51:28 -0400

    chore: Update server.js to serve static files and use root route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 require('dotenv').config({ path: '.env.local' })
 require('colors')
@@ -5,9 +6,8 @@ require('colors')
 const PORT = process.env.PORT || 3500
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('hello')
-})
+app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', require('./routes/root'))
 
 app.listen(PORT, () => {
   console.log(`REPAIR NOTES SERVER RUNNING ON PORT: ${PORT}`.green.inverse)
