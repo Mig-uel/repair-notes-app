b47363367a05490ce82189d6552b5613da85e352
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-19 17:58:49 -0400

    feat: Add Note model for storing user notes

---
+++ models/Note.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const noteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('Note', noteSchema)
